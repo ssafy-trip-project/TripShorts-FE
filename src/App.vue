3e23df71ef13f69bf80de785d186d1a3590b5191
WonJoon <seowj@gmail.com>
2024-11-09 00:48:59 +0900

    CI/CD test

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You Make it!" />
+      <HelloWorld msg="You Did it!" />
 
       <nav>
         <RouterLink to="/main">Home</RouterLink>
